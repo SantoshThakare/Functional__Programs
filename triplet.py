dab2629d2bd967504141e468b4de79b8e4034b25
SantoshThakare <santoshthakare50@gmail.com>
2022-04-18 23:28:07 +0530

    Functional Program Added

---
+++ triplet.py
@@ -0,0 +1,18 @@
+def triples(arr, num):
+    sum = 0
+    for i in range(0, num):
+        for j in range(i + 1, num):
+            for k in range(j + 1, num):
+                sum = arr[i] + arr[j] + arr[k]
+
+                if sum == 0:
+                    print("arr[i]", arr[i], " arr[j] ", arr[j], " arr[k] ", arr[k])
+
+    if sum != 0:
+        print("triplet is not exist")
+
+
+if __name__ == '__main__':
+    arr = [0, -1, 2, -3, 1]
+    num = len(arr)
+    triples(arr, num)
